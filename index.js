3a50e3c38982ab2f2b21ba84a807083df4644cb2
Bryan-2226240020 <161105780+BryanRafferty@users.noreply.github.com>
2024-09-20 11:06:11 +0700

    Update

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const server = http.createServer((req, res) =>{
     // res.writeHead(200, { 'Content-Type': 'text/html'});
     // res.write('Selamat Pagi');
 
-    res.writeHead(200, { 'Content-Type': 'textapplication/json'});
+    res.writeHead(200, { 'Content-Type': 'application/json'});
     res.write(JSON.stringify({
         'message' : 'selamat pagi',
         'status' : 'sukses'
